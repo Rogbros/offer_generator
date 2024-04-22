e4ba9ec2a7b0426c10e7f1e13caa435ad2b30eec
Mateusz Róg <kontakt@matrog.pl>
2024-04-22 21:56:34 +0200

    Fixed decorator issue

--- app/offers/views.py
+++ app/offers/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from allauth.account.decorators import login_required
 
 
 class MainView(TemplateView):
@@ -7,3 +9,7 @@ class MainView(TemplateView):
 
 class OffersView(TemplateView):
     template_name = "offers/pages/offer_view.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
