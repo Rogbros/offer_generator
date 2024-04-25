79866a3b26d68490b1aa06b1f4f8c1429bf40913
Mateusz Róg <kontakt@matrog.pl>
2024-04-25 20:29:03 +0200

    Created working login page

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,13 +1,3 @@
 import '../css/main.css';
 import './alpine.config.js';
 import './htmx.config.js';
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello webpack';
-  return element;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(component());
-});
